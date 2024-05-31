11deec3408aa5fc59edbe5bf26cc3bc0c549aca9
ala <makhloufalaeddine@gmail.com>
2024-05-31 22:57:37 +0100

    added navbar, notfound and home component and added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotfoundComponent,
+    NavbarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
